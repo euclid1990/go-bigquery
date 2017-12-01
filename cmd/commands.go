2ae658a6ceaf2d5bbc271476c8156630d07747a5
Nguyen Van Vuong <euclid1990@gmail.com>
2017-12-01 16:38:22 +0700

    Implement create/query/copy/insert/delete table & view

--- cmd/commands.go
+++ cmd/commands.go
@@ -8,6 +8,7 @@ import (
 	"github.com/euclid1990/go-bigquery/utilities"
 	"golang.org/x/net/context"
 	"os"
+	"time"
 )
 
 // List of options
@@ -35,17 +36,60 @@ func Action(c *cli.Context) {
 		fmt.Printf("Run [All] command.\n")
 	case configs.ACTION_INIT:
 		fmt.Printf("Run [Init] command.\n")
+		fmt.Println("-------------------------------------")
+
 		bigquery := utilities.NewBigQuery(ctx)
 		datasetId := os.Getenv("DATASET_ID")
 		bigquery.CreateDataset(ctx, datasetId)
 
-		userJsonFilePath := utilities.GetUserJsonFilePath()
-		bigquery.CreateTable(ctx, datasetId, s.TABLE_USER, s.User{})
-		bigquery.InsertDataFromFile(ctx, datasetId, s.TABLE_USER, userJsonFilePath)
+		// Create & Load data into 'users' Table
+		if err := bigquery.CreateTable(ctx, datasetId, s.TABLE_USER, s.User{}); err != nil {
+			userJsonFilePath := utilities.GetUserJsonFilePath()
+			bigquery.InsertDataFromFile(ctx, datasetId, s.TABLE_USER, userJsonFilePath)
+		}
 
-		accessJsonFilePath := utilities.GetAccessJsonFilePath()
-		bigquery.CreateTable(ctx, datasetId, s.TABLE_ACCESS, s.Access{})
-		bigquery.InsertDataFromFile(ctx, datasetId, s.TABLE_ACCESS, accessJsonFilePath)
+		// Create & Load data into 'access' Table
+		if err := bigquery.CreateTable(ctx, datasetId, s.TABLE_ACCESS, s.Access{}); err != nil {
+			accessJsonFilePath := utilities.GetAccessJsonFilePath()
+			bigquery.InsertDataFromFile(ctx, datasetId, s.TABLE_ACCESS, accessJsonFilePath)
+		}
+
+		// Copy table access to access1
+		bigquery.CopyTable(ctx, datasetId, s.TABLE_ACCESS, "Access1")
+
+		// List all tables
+		fmt.Println("-------------------------------------")
+		fmt.Printf("Start listing all tables in BigQuery:\n")
+		bigquery.ListTable(ctx, datasetId)
+
+	case "select":
+		bigquery := utilities.NewBigQuery(ctx)
+		datasetId := os.Getenv("DATASET_ID")
+		tableId := "users"
+		r, _ := bigquery.Query(ctx, datasetId, tableId, fmt.Sprintf(`
+			SELECT id, name
+			FROM %s.%s
+			WHERE id >= 90
+		`, datasetId, tableId))
+		for i := 0; i < len(r); i++ {
+			fmt.Println(r[i])
+		}
+
+	case "view":
+		backtick := "`"
+		projectID := os.Getenv("GCP_PROJECT_ID")
+		bigquery := utilities.NewBigQuery(ctx)
+		datasetId := os.Getenv("DATASET_ID")
+		tableId := "users"
+		/* Query on UI
+		#standardSQL
+		SELECT * FROM `bigquery-185608`.kpi.newView LIMIT 1000
+		*/
+		bigquery.CreateView(ctx, datasetId, "newView", fmt.Sprintf(`
+			SELECT id, name
+			FROM %s%s%s.%s.%s
+			WHERE id >= 90
+		`, backtick, projectID, backtick, datasetId, tableId), time.Time{})
 
 	case configs.ACTION_FAKE:
 		fmt.Printf("Run [Fake] command.\n")
--- utilities/bigquery.go
+++ utilities/bigquery.go
@@ -5,7 +5,10 @@ import (
 	"github.com/euclid1990/go-bigquery/configs"
 	"golang.org/x/net/context"
 	"google.golang.org/api/googleapi"
+	"google.golang.org/api/iterator"
 	"os"
+	"reflect"
+	"time"
 )
 
 type BigQuery struct {
@@ -14,13 +17,13 @@ type BigQuery struct {
 }
 
 func NewBigQuery(ctx context.Context) *BigQuery {
-	projectID := os.Getenv("GCP_PROJECT_ID")
-	client, err := bigquery.NewClient(ctx, projectID)
+	projectId := os.Getenv("GCP_PROJECT_ID")
+	client, err := bigquery.NewClient(ctx, projectId)
 	if err != nil {
 		Logf(configs.LOG_ERROR, "Failed to create client: %v", err)
 	}
 	Log(configs.LOG_INFO, "BigQuery client created")
-	return &BigQuery{ProjectId: projectID, Client: client}
+	return &BigQuery{ProjectId: projectId, Client: client}
 }
 
 func (b *BigQuery) CreateDataset(ctx context.Context, datasetId string) error {
@@ -59,6 +62,28 @@ func (b *BigQuery) CreateTable(ctx context.Context, datasetId string, tableId st
 	return nil
 }
 
+func (b *BigQuery) CreateView(ctx context.Context, datasetId string, viewId string, query string, expirationTime time.Time) error {
+	table := b.Client.Dataset(datasetId).Table(viewId)
+	// The time when this table expires. If not set, the table will persist indefinitely.
+	if expirationTime.IsZero() {
+		expirationTime = time.Now().Add(1 * time.Hour)
+	}
+	if err := table.Create(ctx, &bigquery.TableMetadata{
+		Schema:         nil,
+		ViewQuery:      query,
+		ExpirationTime: expirationTime,
+	}); err != nil {
+		if err.(*googleapi.Error).Code == 409 {
+			Logf(configs.LOG_INFO, "View: '%s' has already existed", viewId)
+			return nil
+		}
+		Logf(configs.LOG_ERROR, "Failed to create view %s: %v", viewId, err)
+		return err
+	}
+	Logf(configs.LOG_INFO, "View: '%s' created", viewId)
+	return nil
+}
+
 func (b *BigQuery) InsertDataFromFile(ctx context.Context, datasetId, tableId, filename string) error {
 	f, err := os.Open(filename)
 	if err != nil {
@@ -68,7 +93,7 @@ func (b *BigQuery) InsertDataFromFile(ctx context.Context, datasetId, tableId, f
 	// Config CSV options
 	// source.AllowJaggedRows = true
 	// source.SkipLeadingRows = 1
-	// Using json format: bigquery.JSON/bigquery.CSV
+	// Using json/csv format: bigquery.JSON/bigquery.CSV
 	source.SourceFormat = bigquery.JSON
 
 	loader := b.Client.Dataset(datasetId).Table(tableId).LoaderFrom(source)
@@ -90,4 +115,88 @@ func (b *BigQuery) InsertDataFromFile(ctx context.Context, datasetId, tableId, f
 	}
 	Logf(configs.LOG_INFO, "Inserted data into table: '%s'", tableId)
 	return nil
+}
+
+func (b *BigQuery) ListTable(ctx context.Context, datasetId string) error {
+	tables := b.Client.Dataset(datasetId).Tables(ctx)
+	for {
+		t, err := tables.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		Logf(configs.LOG_INFO, "Table: %s", t.TableID)
+	}
+	return nil
+}
+
+func (b *BigQuery) CopyTable(ctx context.Context, datasetId, srcId, dstId string) error {
+	dataset := b.Client.Dataset(datasetId)
+	copier := dataset.Table(dstId).CopierFrom(dataset.Table(srcId))
+	copier.WriteDisposition = bigquery.WriteTruncate
+	job, err := copier.Run(ctx)
+	if err != nil {
+		return err
+	}
+	status, err := job.Wait(ctx)
+	if err != nil {
+		return err
+	}
+	if err := status.Err(); err != nil {
+		return err
+	}
+	Logf(configs.LOG_INFO, "Table: '%s' has been copied into Table: '%s'", srcId, dstId)
+	return nil
+}
+
+func (b *BigQuery) DeleteTable(ctx context.Context, datasetId, tableId string) error {
+	dataset := b.Client.Dataset(datasetId)
+	if err := dataset.Table(tableId).Delete(ctx); err != nil {
+		if err.(*googleapi.Error).Code == 404 {
+			Logf(configs.LOG_INFO, "Table: '%s' is not existing", tableId)
+			return err
+		}
+		Logf(configs.LOG_ERROR, "Failed to deleted table '%s': %v", tableId, err)
+		return err
+	}
+	Logf(configs.LOG_INFO, "Table: '%s' has been deleted", tableId)
+	return nil
+}
+
+// Type of insert data must be slice/array []*Struct
+func (b *BigQuery) InsertRow(ctx context.Context, datasetId, tableId string, data interface{}) error {
+	u := b.Client.Dataset(datasetId).Table(tableId).Uploader()
+	if err := u.Put(ctx, data); err != nil {
+		Logf(configs.LOG_ERROR, "Failed to insert data into table '%s': %v", tableId, err)
+		return err
+	}
+	srcVal := reflect.ValueOf(data)
+	Logf(configs.LOG_INFO, "Total %d record has been inserted into Table: '%s'", srcVal.Len(), tableId)
+	return nil
+}
+
+func (b *BigQuery) Query(ctx context.Context, datasetId, tableId string, sqlQuery string) ([]interface{}, error) {
+	q := b.Client.Query(sqlQuery)
+	it, err := q.Read(ctx)
+	if err != nil {
+		Logf(configs.LOG_ERROR, "Failed to query table '%s': %v", tableId, err)
+		return nil, err
+	}
+	result := make([]interface{}, 0)
+	for {
+		var row []bigquery.Value
+		err := it.Next(&row)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			Logf(configs.LOG_ERROR, "Failed to map result data from table '%s': %v", tableId, err)
+			return nil, err
+		}
+		result = append(result, row)
+	}
+	Logf(configs.LOG_INFO, "Get total %v records from table '%s', Query took", len(result), tableId)
+	return result, nil
 }
